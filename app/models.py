f96e990886a11a48aa1b350b5e7559aa40ce3e3c
Muhammad Saqlain <saqlainshabbir2023@gmail.com>
2024-10-28 13:46:51 +0500

    13

--- app/models.py
+++ app/models.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 from app import db
 
-# class Subscription(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     name = db.Column(db.String(120), nullable=False)
-#     father_name = db.Column(db.String(120), nullable=False)
-#     phone_number = db.Column(db.String(120), nullable=True)
-#     subscribed_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+class Subscription(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    father_name = db.Column(db.String(120), nullable=False)
+    phone_number = db.Column(db.String(120), nullable=True)
+    subscribed_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 # New schema for storing newsletter details
 class Agriculture_shops(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,27 @@
 from flask import render_template, request, redirect, url_for, flash
 from app import app, db
-from app.models import Agriculture_shops
+from app.models import Subscription, Agriculture_shops
 from sqlalchemy.exc import IntegrityError
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/subscribe', methods=['POST'])
-# def subscribe():
-#     email = request.form['email']
-#     name = request.form['name']
-#     father_name = request.form['father_name']
-#     phone_number = request.form['phone_number']
-#     new_subscription = Subscription(email=email, name=name, father_name=father_name, phone_number=phone_number)
-#     try:
-#         db.session.add(new_subscription)
-#         db.session.commit()
-#         flash('Subscription successful!!', 'success')
-#     except IntegrityError:
-#         db.session.rollback()
-#         flash('This email is already subscribed.', 'error')
-#     return redirect(url_for('index'))
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    email = request.form['email']
+    name = request.form['name']
+    father_name = request.form['father_name']
+    phone_number = request.form['phone_number']
+    new_subscription = Subscription(email=email, name=name, father_name=father_name, phone_number=phone_number)
+    try:
+        db.session.add(new_subscription)
+        db.session.commit()
+        flash('Subscription successful!!', 'success')
+    except IntegrityError:
+        db.session.rollback()
+        flash('This email is already subscribed.', 'error')
+    return redirect(url_for('index'))
 
 @app.route('/Agriculture_shops', methods=['POST'])
 def add_newsletter():
