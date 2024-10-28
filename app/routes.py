82566c0a5312f3663511be0276c94ba49f8f6bdb
Muhammad Saqlain <saqlainshabbir2023@gmail.com>
2024-10-28 15:56:17 +0500

    21

--- app/routes.py
+++ app/routes.py
@@ -59,7 +59,7 @@ def Karyana_shop():
     tehsil = request.form['tehsil']
     address = request.form['address']
     town = request.form['town']
-    new_newsletter = Solar_shops(name=name, phone_number=phone_number, district=district, tehsil=tehsil, address=address, town=town)
+    new_newsletter = Karyana_shops(name=name, phone_number=phone_number, district=district, tehsil=tehsil, address=address, town=town)
     db.session.add(new_newsletter)
     db.session.commit()
     flash('Newsletter created successfully!', 'success')
