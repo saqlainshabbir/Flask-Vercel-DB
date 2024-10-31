61c8f0db959d218f760962d1cdb52caf5fb376af
Muhammad Saqlain <saqlainshabbir2023@gmail.com>
2024-10-31 12:35:38 +0500

    54

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,7 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
