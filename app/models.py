2129886026c26da37b5a7fd5b0f28230e7eebbed
Muhammad Saqlain <saqlainshabbir2023@gmail.com>
2024-11-01 15:08:16 +0500

    62

--- app/models.py
+++ app/models.py
@@ -303,3 +303,14 @@ class Book_centers(db.Model):
     address = db.Column(db.String(200), nullable=False)
     town = db.Column(db.String(50), nullable=False)
     published_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+# New schema for storing Travelling Agencies Data details
+class Travelling_agencies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone_number = db.Column(db.String(15), nullable=False)
+    district = db.Column(db.String(50), nullable=False)
+    tehsil = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    town = db.Column(db.String(50), nullable=False)
+    published_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
