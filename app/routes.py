5c96b920a4ce943887ce12f17dad5b24b76499bd
Muhammad Saqlain <saqlainshabbir2023@gmail.com>
2024-11-04 14:53:37 +0500

    81

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ from app.models import (
     Travelling_agencies, Beauty_parlours, Building_materials, Agriculture_shops,
     Gift_and_toys_shops, Bakeries, Swimming_pools, Cement_shops, Gynaecologists,
     Laboratories, Computers, Mobile_shops, Marbles_shops, Pest_controls, Academies,
-    Shoes_stores, Private_colleges, Private_schools, Private_hospitals
+    Shoes_stores, Private_colleges, Private_schools, Private_hospitals, Orthopedic_hospitals
 )
 from sqlalchemy.exc import IntegrityError
 from werkzeug.utils import secure_filename
@@ -751,4 +751,18 @@ def Private_hospital():
     db.session.add(new_newsletter)
     db.session.commit()
     flash('Private Hospital Added successfully!', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/Orthopedic_hospitals', methods=['POST'])
+def Orthopedic_hospital():
+    name = request.form['name']
+    phone_number = request.form['phone_number']
+    district = request.form['district']
+    tehsil = request.form['tehsil']
+    address = request.form['address']
+    town = request.form['town']
+    new_newsletter = Orthopedic_hospitals(name=name, phone_number=phone_number, district=district, tehsil=tehsil, address=address, town=town)
+    db.session.add(new_newsletter)
+    db.session.commit()
+    flash('Orthopedic Hospital Added successfully!', 'success')
     return redirect(url_for('index'))
